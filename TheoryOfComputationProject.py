cb33a620a8b16277da1accaf3529577e90268c16
Jan Bieniek <46198879+johnnybieniek@users.noreply.github.com>
2023-05-13 09:16:33 -0400

    fixed printing

--- TheoryOfComputationProject.py
+++ TheoryOfComputationProject.py
@@ -1,6 +1,3 @@
-from collections import deque, Counter
-from time import sleep
-
 """
 Rules used in our program:
 
@@ -13,29 +10,29 @@ Rules used in our program:
 6. (qb, e, S) → (qb, e)
 7. (q, $, e) → (q$, e)
 
-
 """
+from collections import deque, Counter
+from time import sleep
 
 
-# function to print a table row
 def print_table(table):
-    for row in table:
-        print(f"{row[0]:<15} {row[1]}")
-    print("-------------------------")
+    print(
+        f"{table[0][0]:<10} {table[0][1]:<10} {table[0][2]:<15} {table[0][3]:<15} {table[0][4]:<10}"
+    )
+    print("-" * 60)
+    for row in table[1:]:
+        print(f"{row[0]:<10} {row[1]:<10} {row[2]:<15} {row[3]:<15} {row[4]:<10}")
     print()
 
 
 def main():
     print("\nWelcome to our program!")
     print("\nIf you want to exit the loop please use 'q' as your input string")
-    print(
-        "This Pushdown Automata can recognize the CFL L={a^n b^n|n>=0} Please input a string to start: "
-    )
+    print("This PDA can recognize L={a^n b^n|n>=0}. Put a string to start: ")
 
     while True:
         step_counter = 0
         stack = []
-
         ruleNumber = ""
         current_state = "p"
         string = input(f"Enter String Here: ")
@@ -44,55 +41,48 @@ def main():
         if "q" in string:
             exit(f"Program finished!")
 
-        # Queue of all the Elements in the Input String
         queue = deque(string)
 
-        # validating the string (number of a's must equal number of b's)
         curr = Counter(string)
         if string and (curr["a"] != curr["b"] or "a" not in curr or "b" not in curr):
-            print("This is an invalid string for L={a^n b^n|n>=0} Try Again!")
+            print("This string is invalid!")
             continue
 
         if step_counter == 0 and len(string) > 0:
-            print(f"========Table for: {string} ========\n")
             table = [
-                ["Step:", step_counter],
-                ["State:", "p"],
-                ["Unread Input:", "".join(list(queue))],
-                ["Stack:", "".join(list(stack[::-1]))],
-                ["Rule number:", "n/a"],
+                ["Step", "State", "Unread Input", "Stack", "Rule number"],
+                [
+                    step_counter,
+                    "p",
+                    "".join(list(queue)),
+                    "".join(list(stack[::-1])),
+                    "n/a",
+                ],
             ]
-            print_table(table)
-            print("-------------------------")
             step_counter += 1
 
         if queue:
             while queue or stack:
-                # Rule 0
                 if current_state == "p":
                     ruleNumber = "#0"
                     current_state = "q"
                     stack.append("s")
 
-                # Rule 1
                 elif current_state == "q" and queue[0] == "a":
                     ruleNumber = "#1"
                     queue.popleft()
                     current_state = "qa"
 
-                # Rule 2
                 elif current_state == "q" and queue[0] == "b":
                     ruleNumber = "#2"
                     queue.popleft()
                     current_state = "qb"
 
-                # Rule 3
                 elif current_state == "qa" and stack[-1] == "a":
                     ruleNumber = "#3"
                     current_state = "q"
                     stack.pop()
 
-                # Rule 4
                 elif current_state == "qa" and stack[-1] == "s":
                     ruleNumber = "#4"
                     stack.pop()
@@ -100,18 +90,15 @@ def main():
                     stack.append("s")
                     stack.append("a")
 
-                # Rule 5
                 elif current_state == "qb" and stack[-1] == "b":
                     ruleNumber = "#5"
                     stack.pop()
                     current_state = "q"
 
-                # Rule 6
                 elif current_state == "qb" and stack[-1] == "s":
                     ruleNumber = "#6"
                     stack.pop()
 
-                # Rule 7
                 elif current_state == "q" and queue[0] == "$":
                     ruleNumber = "#7"
                     current_state = "q$"
@@ -121,46 +108,45 @@ def main():
                     stack.append("e")
                     break
 
-                # Generating a list to print the table after doing a step
-                table = [
-                    ["Step:", step_counter],
-                    ["State:", current_state],
-                    ["Unread Input:", "".join(list(queue))],
-                    ["Stack:", "".join(list(stack[::-1]))],
-                    ["Rule number:", ruleNumber],
-                ]
-                print_table(table)
-                print("-------------------------")
+                table.append(
+                    [
+                        step_counter,
+                        current_state,
+                        "".join(list(queue)),
+                        "".join(list(stack[::-1])),
+                        ruleNumber,
+                    ]
+                )
                 step_counter += 1
 
-            # Generating a list to print the table once the remaining string is empty
-            table = [
-                ["Step:", step_counter],
-                ["State:", current_state],
-                ["Unread Input:", "".join(list(queue))],
-                ["Stack:", "".join(list(stack[::-1]))],
-                ["Rule number:", ruleNumber],
-            ]
+            table.append(
+                [
+                    step_counter,
+                    current_state,
+                    "".join(list(queue)),
+                    "".join(list(stack[::-1])),
+                    ruleNumber,
+                ]
+            )
             print_table(table)
-            print("-------------------------")
         else:
-            # Generating a list to print the table if the inputed string was an empty string
             if not string:
                 queue.append("e")
                 stack.append("e")
                 current_state = "q"
 
                 table = [
-                    ["Step:", step_counter],
-                    ["State:", current_state],
-                    ["Unread Input:", "".join(list(queue))],
-                    ["Stack:", "".join(list(stack[::-1]))],
-                    ["Rule number:", ruleNumber],
+                    ["Step", "State", "Unread Input", "Stack", "Rule number"],
+                    [
+                        step_counter,
+                        current_state,
+                        "".join(list(queue)),
+                        "".join(list(stack[::-1])),
+                        ruleNumber,
+                    ],
                 ]
                 print_table(table)
 
 
 if __name__ == "__main__":
     main()
-
-    
